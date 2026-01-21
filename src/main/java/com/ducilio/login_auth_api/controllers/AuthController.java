9fa6e0c94ad5bc9bbb21a50f0c13a75b08166530
marcilio11-du <marcilio11du@gmail.com>
2026-01-22 00:43:56 +0100

    refacot: Solve Cors Problem... kkkkkkkk

--- src/main/java/com/ducilio/login_auth_api/controllers/AuthController.java
+++ src/main/java/com/ducilio/login_auth_api/controllers/AuthController.java
@@ -9,16 +9,14 @@ import com.ducilio.login_auth_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
